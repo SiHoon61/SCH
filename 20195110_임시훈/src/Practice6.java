8dee7dcb2fc493413090f6d7d9324e030e90c184
dlatl <dlatl@DESKTOP-NHRML8J>
2020-06-02 19:24:02 +0900

    aa

---
+++ 20195110_임시훈/src/Practice6.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Practice6 extends JFrame{
+	public CheckBoxEx() {
+		setTitle("시훈");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+	}
+	public static void main(String[] args) {
+		d
+	}
+
+}
